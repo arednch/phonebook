5bf8372657548fe91b7b84b767d0fbe68d2574da
Martin Suess <martin.suess@kun.ch>
2024-06-08 17:29:57 +0200

    Revert "minore improvement on private check"
    This reverts commit f0b41b051ffff1a94cb20c22a5819277bbe79b4d.

--- importer/import.go
+++ importer/import.go
@@ -12,8 +12,7 @@ import (
 )
 
 const (
-	eofSignal   = "ENDOFFILE"
-	privateMark = "Y"
+	eofSignal = "ENDOFFILE"
 )
 
 func ReadFromURL(url string) ([]byte, error) {
@@ -74,7 +73,7 @@ func ReadPhonebook(path string) ([]*data.Entry, error) {
 			break
 		}
 		// check if entry is marked as private and if so, skip it
-		if len(r) > 11 && strings.EqualFold(strings.TrimSpace(r[11]), privateMark) {
+		if len(r) > 11 && strings.TrimSpace(r[11]) == "Y" {
 			continue
 		}
 

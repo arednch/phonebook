e1a00b863c1ff2e8ca856a66fc936423dc620d2c
Martin Suess <martin.suess@kun.ch>
2024-06-23 22:56:32 +0200

    testable version of minimalistic SIP implementation

--- data/sip.go
+++ data/sip.go
@@ -3,15 +3,11 @@ package data
 import (
 	"bufio"
 	"bytes"
+	"errors"
 	"fmt"
 	"strconv"
 	"strings"
 )
-
-type SIPMessage interface {
-	Parse([]byte) error
-	Serialize() ([]byte, error)
-}
 
 type SIPRequest struct {
 	Method     string
@@ -21,6 +17,26 @@ type SIPRequest struct {
 
 	// Not implemented at least for now.
 	// Body []byte
+}
+
+func (r *SIPRequest) From() *SIPAddress {
+	for _, hdr := range r.Headers {
+		if strings.ToLower(hdr.Name) != "from" {
+			continue
+		}
+		return hdr.Address
+	}
+	return nil
+}
+
+func (r *SIPRequest) To() *SIPAddress {
+	for _, hdr := range r.Headers {
+		if strings.ToLower(hdr.Name) != "to" {
+			continue
+		}
+		return hdr.Address
+	}
+	return nil
 }
 
 func (r *SIPRequest) Parse(data []byte) error {
@@ -114,7 +130,7 @@ type SIPResponse struct {
 	StatusMessage string
 	Headers       []*SIPHeader
 
-	Body string
+	Body []byte
 }
 
 func (r *SIPResponse) Serialize() []byte {
@@ -139,8 +155,8 @@ func (r *SIPResponse) Serialize() []byte {
 	buf.WriteString("\r\n")
 
 	// Body
-	if r.Body != "" {
-		buf.WriteString(r.Body)
+	if r.Body != nil {
+		buf.Write(r.Body)
 	}
 
 	return buf.Bytes()
@@ -149,6 +165,8 @@ func (r *SIPResponse) Serialize() []byte {
 type SIPHeader struct {
 	Name  string
 	Value string
+
+	Address *SIPAddress // Optionally set when header has an address.
 }
 
 func (h *SIPHeader) Clone() SIPHeader {
@@ -167,9 +185,149 @@ func (h *SIPHeader) parse(line string) error {
 	h.Name = strings.TrimSpace(line[:idx])
 	h.Value = strings.TrimSpace(line[idx+1:])
 
+	switch strings.ToLower(h.Name) {
+	case "to", "from", "contact":
+		addr := &SIPAddress{}
+		if err := addr.Parse(h.Value); err == nil {
+			h.Address = addr
+		}
+	}
+
 	return nil
 }
 
 func (h *SIPHeader) serialize() string {
 	return fmt.Sprintf("%s: %s", h.Name, h.Value)
+}
+
+type SIPAddress struct {
+	DisplayName string
+	URI         *SIPURI
+	Params      map[string]string
+}
+
+func (a *SIPAddress) String() string {
+	return a.URI.String()
+}
+
+func (a *SIPAddress) Parse(line string) error {
+	l := strings.TrimSpace(line)
+	if l == "" {
+		return errors.New("empty address")
+	}
+
+	var uri string
+	a.DisplayName, uri = findDisplayName(l)
+	a.URI = parseSIPURI(uri)
+
+	return nil
+}
+
+// https://datatracker.ietf.org/doc/html/rfc3261#section-19.1.1
+// sip:user:password@host:port;uri-parameters?headers
+func parseSIPURI(l string) *SIPURI {
+	l = strings.TrimSpace(l)
+	l = strings.TrimPrefix(l, "sip:")
+	l = strings.Split(l, ";")[0] // ignore parameters for now
+
+	uri := &SIPURI{}
+	parts := strings.Split(l, "@")
+	if len(parts) > 1 {
+		user := strings.Split(parts[0], ":")
+		uri.User = user[0] // ignoring the possibility that there may be a password
+
+		host := strings.Split(parts[1], ":")
+		uri.Host = host[0]
+		if len(host) > 1 {
+			p, _ := strconv.Atoi(host[1])
+			uri.Port = p
+		}
+	} else {
+		host := strings.Split(parts[0], ":")
+		uri.Host = host[0]
+		if len(host) > 1 {
+			p, _ := strconv.Atoi(host[1])
+			uri.Port = p
+		}
+	}
+
+	return uri
+}
+
+func findDisplayName(l string) (string, string) {
+	startQuote := -1
+	endQuote := -1
+	for i, c := range l {
+		if c == '"' {
+			if startQuote < 0 {
+				startQuote = i
+			} else {
+				endQuote = i
+			}
+			continue
+		}
+
+		// https://datatracker.ietf.org/doc/html/rfc3261#section-20.10
+		// When the header field value contains a display name, the URI
+		// including all URI parameters is enclosed in "<" and ">".  If no "<"
+		// and ">" are present, all parameters after the URI are header
+		// parameters, not URI parameters.
+		if c == '<' {
+			dn := strings.TrimSpace(l[:i])
+			uri := l[i+1:]
+			if endQuote > 0 {
+				dn = l[startQuote+1 : endQuote]
+			}
+
+			for i, c := range uri {
+				if c == '>' {
+					uri = uri[:i]
+					break
+				}
+			}
+
+			return dn, uri
+		}
+
+		if c == ';' {
+			if startQuote > 0 {
+				continue
+			}
+			// detect early
+			// uri can be without <> in that case there all after ; are header params
+			return "", findURI(l)
+		}
+	}
+	return "", findURI(l)
+}
+
+func findURI(l string) string {
+	for i, c := range l {
+		if c == ';' {
+			return l[:i]
+		}
+	}
+	return l
+}
+
+func (a *SIPAddress) Serialize() []byte {
+	return nil
+}
+
+type SIPURI struct {
+	User   string // The user part of the URI. The 'joe' in sip:joe@example.com
+	Host   string // The host part of the URI. This can be a domain, or a string representation of an IP address.
+	Port   int    // The port part of the URI. This is optional, and can be empty.
+	Params map[string]string
+}
+
+func (u *SIPURI) String() string {
+	host := u.Host
+	if u.Port > 0 {
+		host = fmt.Sprintf("%s:%d", host, u.Port)
+	}
+	if u.User == "" {
+		return fmt.Sprintf("<sip:%s>", host)
+	}
+	return fmt.Sprintf("<sip:%s@%s>", u.User, host)
 }
--- sip/server.go
+++ sip/server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"strings"
 
 	"github.com/arednch/phonebook/configuration"
 	"github.com/arednch/phonebook/data"
@@ -46,8 +47,6 @@ func (s *Server) ListenAndServe(ctx context.Context, proto, addr string) error {
 				return
 			}
 
-			r := resp.Serialize()
-			fmt.Printf("%s\n", r)
 			pc.WriteTo(resp.Serialize(), addr)
 		}(pc, addr, buf[:n])
 	}
@@ -85,65 +84,63 @@ func (s *Server) handleBye(req *data.SIPRequest) (*data.SIPResponse, error) {
 }
 
 func (s *Server) handleInvite(req *data.SIPRequest) (*data.SIPResponse, error) {
-	// 	if s.Config.Debug {
-	// 		fmt.Printf("SIP/INVITE: received INVITE message from %q to %q\n", req.From(), req.To())
-	// 	}
+	if s.Config.Debug {
+		fmt.Printf("SIP/INVITE: received INVITE message from %q to %q\n", req.From(), req.To())
+	}
 
-	// 	// Check if this is a call directed at a local identity (hostname or IP). If not, ignore it.
-	// 	// This also helps reducing retry storms for some clients (e.g. Linphone).
-	// 	if s.LocalIdentities != nil {
-	// 		if local, ok := s.LocalIdentities[strings.ToLower(req.To().Address.Host)]; !ok || !local {
-	// 			if err := tx.Respond(sip.NewResponseFromRequest(req, sip.StatusNotFound, "Not Found", nil)); err != nil {
-	// 				fmt.Printf("SIP/INVITE: error sending response: %s\n", err)
-	// 			}
-	// 			return
-	// 		}
-	// 	}
+	// Check if this is a call directed at a local identity (hostname or IP). If not, ignore it.
+	// This also helps reducing retry storms for some clients (e.g. Linphone).
+	if s.LocalIdentities != nil {
+		if local, ok := s.LocalIdentities[strings.ToLower(req.To().URI.Host)]; !ok || !local {
+			return data.NewSIPResponseFromRequest(req, http.StatusNotFound, "Not Found"), nil
+		}
+	}
 
-	// 	// Look up the phone number and try to find the right host in our records and redirect the call there.
-	// 	var redirect *sip.Uri
-	// 	to := req.To()
-	// 	s.Records.Mu.RLock()
-	// 	for _, entry := range s.Records.Entries {
-	// 		if entry.PhoneNumber != to.Address.User {
-	// 			continue
-	// 		}
+	// Look up the phone number and try to find the right host in our records and redirect the call there.
+	var redirect *data.SIPURI
+	to := req.To()
+	s.Records.Mu.RLock()
+	for _, entry := range s.Records.Entries {
+		if entry.PhoneNumber != to.URI.User {
+			continue
+		}
 
-	// 		host := fmt.Sprintf("%s.local.mesh", entry.PhoneNumber)
-	// 		parts := strings.Split(entry.IPAddress, data.SIPSeparator)
-	// 		if len(parts) > 1 {
-	// 			if s.Config.Resolve && entry.OLSR != nil {
-	// 				host = entry.OLSR.IP
-	// 			} else {
-	// 				host = parts[1]
-	// 			}
-	// 		}
+		host := fmt.Sprintf("%s.local.mesh", entry.PhoneNumber)
+		parts := strings.Split(entry.IPAddress, data.SIPSeparator)
+		if len(parts) > 1 {
+			if s.Config.Resolve && entry.OLSR != nil {
+				host = entry.OLSR.IP
+			} else {
+				host = parts[1]
+			}
+		}
 
-	// 		// We found an entry in the phonebook to redirect to.
-	// 		redirect = &sip.Uri{
-	// 			User: entry.PhoneNumber,
-	// 			Host: host,
-	// 			// Port: 5060,
-	// 			// UriParams: sip.HeaderParams{
-	// 			// 	"Transport": "udp",
-	// 			// },
-	// 		}
-	// 		break
-	// 	}
-	// 	s.Records.Mu.RUnlock()
+		// We found an entry in the phonebook to redirect to.
+		redirect = &data.SIPURI{
+			User: entry.PhoneNumber,
+			Host: host,
+		}
+		break
+	}
+	s.Records.Mu.RUnlock()
 
-	// 	if redirect != nil {
-	// 		resp := sip.NewResponseFromRequest(req, sip.StatusMovedTemporarily, "Moved Temporarily", nil)
-	// 		resp.AppendHeaderAfter(&sip.ContactHeader{
-	// 			DisplayName: redirect.User,
-	// 			Address:     *redirect,
-	// 		}, "To")
-	// 		resp.AppendHeaderAfter(sip.NewHeader("Diversion", fmt.Sprintf("\"%s\" <%s>;reason=unconditional", redirect.User, redirect.String())), "To")
-	// 		if err := tx.Respond(resp); err != nil {
-	// 			fmt.Printf("SIP/INVITE: error sending response: %s\n", err)
-	// 		}
-	// 		return
-	// 	}
+	if redirect != nil {
+		resp := data.NewSIPResponseFromRequest(req, http.StatusFound, "Moved Temporarily")
+		resp.Headers = append(resp.Headers, &data.SIPHeader{
+			Name:  "Contact",
+			Value: redirect.String(),
+			Address: &data.SIPAddress{
+				DisplayName: redirect.User,
+				URI:         redirect,
+			},
+		})
+		resp.Headers = append(resp.Headers, &data.SIPHeader{
+			Name:  "Diversion",
+			Value: fmt.Sprintf("\"%s\" <%s>;reason=unconditional", redirect.User, redirect.String()),
+		})
+
+		return resp, nil
+	}
 
 	// As a last resort, we're giving up and tell the client that we can't route that call.
 	return data.NewSIPResponseFromRequest(req, http.StatusNotFound, "Not Found"), nil

126662586724e199bfee45951b8d971826a70015
Martin Suess <martin.suess@kun.ch>
2024-07-13 12:38:09 +0200

    clarify message types

--- data/updates.go
+++ data/updates.go
@@ -13,6 +13,14 @@ type Updates struct {
 }
 
 type Update struct {
-	Type    string `json:"info_type"`
+	// Type defines what color the message is rendered in. Supported are:
+	// - "info": Light blue background.
+	// - "warn": Yellow background.
+	// - "danger": Dark red background.
+	// - "success": Dark green background.
+	// - every other value will be rendered with dark grey background.
+	// For more details, see https://getbootstrap.com/docs/5.3/components/alerts/
+	Type string `json:"info_type"`
+
 	Message string `json:"message"`
 }
